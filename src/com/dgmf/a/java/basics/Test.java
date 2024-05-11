f49e74531919cbfc4b3cfd4653891d0a7f908e88
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-11 07:21:35 +0200

    First Java Program

---
+++ src/com/dgmf/a/java/basics/Test.java
@@ -0,0 +1,11 @@
+package com.dgmf.a.java.basics;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// Print Something to the Screen
+		System.out.println("Hello, the Weather is Great ...");
+
+	}
+
+}
